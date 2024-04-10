85978169870b87e792a242c24ce47ddff47d3253
Noah Timmermann <noahtimmermann5@gmail.com>
2024-04-10 22:11:47 +0200

    Little update

--- Functions/game.py
+++ Functions/game.py
@@ -7,6 +7,7 @@ from main import main
 def print_rules():
     print("\n----------------Bienvenue dans notre jeu du pendu en Python !----------------")
     print("\n---------------------------Noah, Matthew et Yollan---------------------------")
+    print("\n----------------------https://github.com/snake-yt/hangman--------------------")
     print("\nLes règles sont simples :")
     print("\n> 1. Vous devez créer vos utilisateurs, ou un utisateur peut jouer contre un bot.")
     print("> 2. Un mot est choisi dans une liste prédéfinie, et vous devez le deviner chacun votre tour.")
